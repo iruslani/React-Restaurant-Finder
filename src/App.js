5f550cf94b40905baf77fc486bd14c285c0a0e7a
iruslani <iruslani75@gmail.com>
2016-09-08 14:30:55 -0700

    Removed unused html from sample bootstrap code.

--- src/App.js
+++ src/App.js
@@ -32,11 +32,6 @@ class RestaurantList extends Component {
 				{restaurants.map(function(restaurants){
 						return <li key={restaurants.id} className="list-group-item">Name: {restaurants.Title} - City: {restaurants.City}</li>
 				})}
-        {/*<li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>*/}
       </ul>
     )
   }
