732a7ff48185c3f2b45193951d311b76a9b09413
iruslani <iruslani75@gmail.com>
2016-09-10 22:35:01 -0700

    Fixed restaurant sorting.

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,61 +1,64 @@
 import React, { Component } from 'react';
-import 'whatwg-fetch';
+import RestaurantList from './RestaurantList';
 import {checkStatus, parseJSON} from '../utilities/index';
-
-class RestaurantList extends Component {
-	constructor(props) {
-    super(props);
-    this.state = {
-      restaurants: []
-    }
-  }
-	componentDidMount() {
-		var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search%20where%20zip%3D'94085'%20and%20query%3D'pizza'&format=json&diagnostics=true&callback="
-		fetch(url)
-			.then(checkStatus)
-			.then(parseJSON)
-			.then(function(data) {
-				console.log('request succeeded with JSON response', data.query.results.Result)
-				var results = data.query.results.Result
-				this.setState({restaurants :  results});
-			}.bind(this)).catch(function(error) {
-				console.log('request failed', error)
-			})
-	}
-	render() {
-		console.log(this.state)
-		console.log(this.props)
-		var restaurants = this.state.restaurants || [];
-		return (
-			<div>
-				{this.props.sort}
-				<p>{this.props.query}</p>
-				<ul className="list-group">
-					{restaurants.map(function(restaurant){
-							return <li key={restaurant.id} className="list-group-item">Name: {restaurant.Title} - City: {restaurant.City}</li>
-					})}
-				</ul>
-			</div>
-		)
-	}
-}
 
 class RestaurantContainer extends Component {
 	constructor(props) {
     super(props);
     this.state = {
-      restaurants: [],
-			sort: 'distance',
+			restaurants : [],
+			sort: 'ratings',
 			query: this.props.query
     };
 		this.toggleSortRatings = this.toggleSortRatings.bind(this);
 		this.toggleSortDistance = this.toggleSortDistance.bind(this);
   }
+	componentDidMount() {
+		let url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search%20WHERE%20latitude%3D%2237.6536537%22%20and%20longitude%3D%22-122.4656777%22%20and%20query%3D'"+this.props.query+"'%20and%20radius%3D%2250%22&format=json&diagnostics=true&callback="
+		// let url = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20local.search%20WHERE%20latitude%3D%2237.6536537%22%20and%20longitude%3D%22-122.4656777%22%20and%20query%3D%22pizza%22%20and%20radius%3D%2220%22&format=json&diagnostics=true&callback="
+		fetch(url)
+			.then(checkStatus)
+			.then(parseJSON)
+			.then(function(data) {
+				console.log('request succeeded with JSON response', data.query.results.Result)
+				let results = data.query.results.Result
+				results = data.query.results.Result.sort((a, b) => {
+					if(!isFinite(a.Rating.AverageRating-b.Rating.AverageRating))
+						 return !isFinite(a.Rating.AverageRating) ? 1 : -1;
+					else
+						// return a.Distance - b.Distance;
+						 return b.Rating.AverageRating - a.Rating.AverageRating;
+				});
+				this.setState({restaurants :  results});
+			}.bind(this)).catch(function(error) {
+				console.log('request failed', error)
+			})
+	}
 	toggleSortDistance() {
-		this.setState({sort :'distance'});
+		let results = this.state.restaurants
+		results = this.state.restaurants.sort((a, b) => {
+			if(!isFinite(a.Distance-b.Distance))
+				 return !isFinite(a.Distance) ? 1 : -1;
+			else
+				 return a.Distance - b.Distance;
+		});
+		this.setState({
+			sort :'distance',
+			restaurants :  results
+		});
 	}
 	toggleSortRatings() {
-		this.setState({sort :'ratings'});
+		let results = this.state.restaurants
+		results = this.state.restaurants.sort((a, b) => {
+			if(!isFinite(a.Rating.AverageRating-b.Rating.AverageRating))
+				 return !isFinite(a.Rating.AverageRating) ? 1 : -1;
+			else
+				 return b.Rating.AverageRating - a.Rating.AverageRating;
+		});
+		this.setState({
+			sort :'ratings',
+			restaurants :  results
+		});
 	}
 	render() {
 
@@ -64,10 +67,10 @@ class RestaurantContainer extends Component {
 				<h3>Suggested restaurants:</h3>
 				<p>Sort by:</p>
 				<ul className="nav nav-pills">
+					<li role="presentation" onClick={this.toggleSortRatings} className={this.state.sort === 'ratings' ? 'active' : ''}><a href="#">Ratings</a></li>
 				  <li role="presentation" onClick={this.toggleSortDistance} className={this.state.sort === 'distance' ? 'active' : ''}><a href="#">Distance</a></li>
-				  <li role="presentation" onClick={this.toggleSortRatings} className={this.state.sort === 'ratings' ? 'active' : ''}><a href="#">Ratings</a></li>
 				</ul>
-				<RestaurantList sort={this.state.sort} query={this.state.query}/>
+				<RestaurantList sort={this.state.sort} restaurants={this.state.restaurants} query={this.state.query}/>
 			</div>
     )
   }
---
+++ src/components/RestaurantList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+// import {checkStatus, parseJSON} from '../utilities/index';
+
+class RestaurantList extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: this.props.restaurants
+    };
+  }
+	render() {
+		console.log(this.state)
+		console.log(this.props)
+		var restaurants = this.props.restaurants || [];
+		return (
+			<div>
+				<p>Sort by: {this.props.sort}</p>
+				<p>Keyword: {this.props.query}</p>
+				<ul className="list-group">
+					{restaurants.map(function(restaurant){
+							return <li key={restaurant.id} className="list-group-item">
+												{restaurant.Title} <br/>
+											  City: {restaurant.City} <br/>
+												Distance: {restaurant.Distance} <br/>
+												Ratings: {restaurant.Rating.AverageRating !== 'NaN' ? restaurant.Rating.AverageRating : '0'}
+											</li>
+					})}
+				</ul>
+			</div>
+		)
+	}
+}
+export default RestaurantList;
