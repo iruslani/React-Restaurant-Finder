1b782d33b9842fb52ef563f21ac352de56530574
iruslani <iruslani75@gmail.com>
2016-09-11 02:19:00 -0700

    Cleaned up RestaurantList component and converted it to stateless.

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -3,26 +3,21 @@ import 'whatwg-fetch';
 // import {checkStatus, parseJSON} from '../utilities/index';
 
 class RestaurantList extends Component {
-	constructor(props) {
-    super(props);
-    this.state = {
-      restaurants: this.props.restaurants
-    };
-  }
 	render() {
-		console.log(this.state)
-		console.log(this.props)
 		var restaurants = this.props.restaurants || [];
 		return (
 			<div>
 				<ul className="list-group">
 					{restaurants.map(function(restaurant){
-							return <li key={restaurant.id} className="list-group-item">
-												{restaurant.Title} <br/>
-											  City: {restaurant.City} <br/>
-												Distance: {restaurant.Distance} <br/>
-												Ratings: {restaurant.Rating.AverageRating !== 'NaN' ? restaurant.Rating.AverageRating : '0'}
-											</li>
+						return (
+							<li key={restaurant.id} className="list-group-item">
+								{restaurant.Title} <br/>
+								City: {restaurant.City} <br/>
+								Distance: {restaurant.Distance} <br/>
+								Ratings: {restaurant.Rating.AverageRating !== 'NaN' ? restaurant.Rating.AverageRating : '0'}
+							</li>
+						)
+
 					})}
 				</ul>
 			</div>
