0c3ebef94f3fcf6e547630962d72935174400341
iruslani <iruslani75@gmail.com>
2016-11-05 20:13:31 -0700

    changed directory name in express app.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', express.static(path.join(__dirname, 'build')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
